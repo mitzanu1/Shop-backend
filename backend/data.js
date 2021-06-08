a579d189a7161f661cac8c2723681d6296e915a7
mitzanu <mitzanu@hotmail.com>
2021-06-09 00:17:34 +0300

    initial

---
+++ backend/data.js
@@ -0,0 +1,75 @@
+
+
+export default {
+
+   
+
+    products: [
+        {
+            name:'Shirt 1',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'60',
+            rating:'3.5',
+            numReviews: '12',
+            image:'/images/product-1.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 4
+        },
+        {
+            name:'Shirt 2',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'160',
+            rating:'4',
+            numReviews: '15',
+            image:'/images/product-2.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 200
+        },
+        {
+            name:'Shirt 3',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'80',
+            rating:'3',
+            numReviews: '8',
+            image: '/images/product-3.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 5
+        },
+        {
+            name:'Pants 1',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'120',
+            rating:'4.5',
+            numReviews: '11',
+            image:'/images/product-4.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 0
+        },
+        {
+            name:'Pants 2',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'54',
+            rating:'5',
+            numReviews: '18',
+            image: '/images/product-5.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 0
+        },
+        {
+            name:'Pants 3',
+            category: 'Shirt',
+            brand:'Nike',
+            price:'76',
+            rating:'1.5',
+            numReviews: '3',
+            image: '/images/product-6.jpg',
+            description: 'Lorem ipsum dolor sit amet.',
+            inStock: 8
+        },
+    ]
+}
