7b0aca7e1335b3988c03a46a8ca25a82940aaad3
mitzanu <mitzanu@hotmail.com>
2021-06-09 23:57:39 +0300

    i

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,17 @@ import productRouter from './routers/productRouter.js'
 import userRouter from './routers/userRouter.js'
 import dotenv from 'dotenv'
 import orderRouter from './routers/orderRouter.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const dbUrl = 'mongodb+srv://mitzanu:250786aa@cluster1.j5uhq.mongodb.net/eShop?retryWrites=true&w=majority';
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(dbUrl || 'https://localhost/eShop', {
+mongoose.connect(dbUrl , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
